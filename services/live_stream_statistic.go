e259548c50bf7f05d839145e6a34853c0f149e3f
dong.van.le <dong.van.le@aioz.io>
2025-04-22 14:23:33 +0700

    ref: code

--- services/live_stream_statistic.go
+++ services/live_stream_statistic.go
@@ -15,28 +15,28 @@ func NewStatisticService(repo LiveStreamStatisticRepository) {
 }
 
 type LiveStreamStatisticRepository interface {
-	UpsertBitrateIn(stream_key uuid.UUID, bitrate float64) error
-	UpsertBitrateOut(stream_key uuid.UUID, bitrate float64) error
-	UpsertFPSIn(stream_key uuid.UUID, fps int16) error
-	UpsertFPSOut(stream_key uuid.UUID, fps int16) error
-	UpsertNumberOfRequests(stream_key uuid.UUID, number_of_requests int) error
-	UpsertDataTransferred(stream_key uuid.UUID, data_transferred float64) error
-	UpsertDevice(stream_key uuid.UUID, device string) error
-	UpsertOS(stream_key uuid.UUID, os string) error
-	UpsertLocation(stream_key uuid.UUID, location string) error
+	UpsertBitrateIn(streamKey uuid.UUID, bitrate float64) error
+	UpsertBitrateOut(streamKey uuid.UUID, bitrate float64) error
+	UpsertFPSIn(streamKey uuid.UUID, fps int16) error
+	UpsertFPSOut(streamKey uuid.UUID, fps int16) error
+	UpsertNumberOfRequests(streamKey uuid.UUID, numberOfRequests int) error
+	UpsertDataTransferred(streamKey uuid.UUID, dataTransferred float64) error
+	UpsertDevice(streamKey uuid.UUID, device string) error
+	UpsertOS(streamKey uuid.UUID, os string) error
+	UpsertLocation(streamKey uuid.UUID, location string) error
 }
 
-func (s *StatisticService) UpsertBitrateOut(stream_key uuid.UUID, bitrate float64) error {
-	return s.livestreamStatisticRepo.UpsertBitrateOut(stream_key, bitrate)
+func (s *StatisticService) UpsertBitrateOut(streamKey uuid.UUID, bitrate float64) error {
+	return s.livestreamStatisticRepo.UpsertBitrateOut(streamKey, bitrate)
 }
 
-func (s *StatisticService) UpsertDataTransferred(stream_key uuid.UUID, dataTransfer float64) error {
-	return s.livestreamStatisticRepo.UpsertDataTransferred(stream_key, dataTransfer)
+func (s *StatisticService) UpsertDataTransferred(streamKey uuid.UUID, dataTransfer float64) error {
+	return s.livestreamStatisticRepo.UpsertDataTransferred(streamKey, dataTransfer)
 }
-func (s *StatisticService) UpsertFPSOut(stream_key uuid.UUID, fps int16) error {
-	return s.livestreamStatisticRepo.UpsertFPSOut(stream_key, fps)
+func (s *StatisticService) UpsertFPSOut(streamKey uuid.UUID, fps int16) error {
+	return s.livestreamStatisticRepo.UpsertFPSOut(streamKey, fps)
 }
 
-func (s *StatisticService) UpsertFPSIn(stream_key uuid.UUID, fps int16) error {
-	return s.livestreamStatisticRepo.UpsertFPSIn(stream_key, fps)
+func (s *StatisticService) UpsertFPSIn(streamKey uuid.UUID, fps int16) error {
+	return s.livestreamStatisticRepo.UpsertFPSIn(streamKey, fps)
 }
